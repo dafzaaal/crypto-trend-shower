c5e336c6debc284a6287597c2dc8a872d2c5a9fb
Dawood Afzaal <dawoodafzaal13@gmail.com>
2023-08-06 21:25:18 -0600

    initial commit to repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/styles.css';
+import React from 'react';
+import bitcoin from './images/bitcoin.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <html>
+      <head>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+      <link href="https://fonts.googleapis.com/css2?family=Asap:wght@100&family=IBM+Plex+Sans:wght@100&family=Noto+Sans+JP:wght@100&family=Nunito:wght@200&family=Open+Sans:wght@300&family=Roboto:wght@100&family=Space+Mono&display=swap" rel="stylesheet"/>
+      <title>Cyrpto Currency Trend Graph</title>
+      </head>
+
+
+
+      <body>
+
+        <div class="intro">
+          <h2>Crypto-Tracker!</h2>
+          <p>
+          This is an application which when given a valid crypto currency name will return a graph visualizing 
+          it's trend over the last 7-days!
+          </p>
+        </div>
+
+
+
+
+
+        <div class="container">
+          <input type='text' name='crypto name' id="cryptoName" placeholder="Enter a crypto name..." />
+        </div>
+      </body>
+
+
+
+
+    </html>
   );
 }
 
